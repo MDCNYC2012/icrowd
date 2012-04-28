4d5e8c7e604bfb82bd883633c9e8c95f69ea93d7
outrightmental <nick.c.kaye@gmail.com>
2012-04-28 17:24:37 -0400

    laid the framework for a very simple jQuery app

---
+++ js-client/app/icrowd.js
@@ -0,0 +1,18 @@
+/*
+ * js file used for iCrowd client application
+ **/
+
+(function($) {
+    /************************ Public functions - begin ************************/
+    $.icrowd = {
+        init: function(){
+            __initView();
+        }
+    };
+
+    /**************************** Private functions ****************************/
+    function initView() {
+        alert("we got view");
+    }
+
+})(jQuery);
---
+++ js-client/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link href="css/reset.css" rel="stylesheet" type="text/css" />
+    <link href="css/icrowd.css" rel="stylesheet" type="text/css" />
+    <script src="lib/jquery-1.7.2.min.js" language="javascript" ></script>
+    <script src="app/icrowd.js" language="javascript" ></script>
+</head>
+<body>
+<script language="JavaScript">
+    $(document).ready(function()
+    {
+        icrowd.init();
+    });
+</script>
+</body>
+</html>
