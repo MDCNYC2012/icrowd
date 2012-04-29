64c5339acbc5db294b428898edf47a5b148dfada
outrightmental <nick.c.kaye@gmail.com>
2012-04-29 08:05:46 -0400

    borrowed code to programmatically obtain my ip address from:  http://blog.zachwaugh.com/post/309927273/programmatically-retrieving-ip-address-of-iphone

---
+++ ios-host/icrowd/Classes/icConnectionManager.m
@@ -0,0 +1,69 @@
+//
+//  icConnectionManager.m
+//  icrowd
+//
+//  Created by Nick Kaye on 4/29/12.
+//  Copyright (c) 2012 Outright Mental. All rights reserved.
+//
+
+#import "icConnectionManager.h"
+#include <ifaddrs.h>
+#include <arpa/inet.h>
+
+@implementation icConnectionManager
+
+/*
+ */
+#pragma mark properties
+
+@synthesize netstatViewDelegate = _netstatViewDelegate;
+
+/*
+ */
+#pragma mark singleton
+
+static id __instance;
+
++(icConnectionManager *) singleton
+{
+    if (__instance!=nil) return __instance;
+    __instance = [icConnectionManager new];
+    return __instance;
+}
+
+- (NSString *)getIPAddress
+{
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    
+    // retrieve the current interfaces - returns 0 on success
+    success = getifaddrs(&interfaces);
+    if (success == 0)
+    {
+        // Loop through linked list of interfaces
+        temp_addr = interfaces;
+        while(temp_addr != NULL)
+        {
+            if(temp_addr->ifa_addr->sa_family == AF_INET)
+            {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"])
+                {
+                    // Get NSString from C String
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                }
+            }
+            
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    
+    // Free memory
+    freeifaddrs(interfaces);
+    
+    return address;
+}
+
+@end
